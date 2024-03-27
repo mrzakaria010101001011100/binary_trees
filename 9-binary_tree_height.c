004f6bc9343f48d018ee5bfcc6f50cc483e8aa9d
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-03-27 05:34:17 -0700

    binary_trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,16 +7,23 @@
  *
  * Return: Height of the tree, or 0 if tree is NULL
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t l = 0;
+	size_t r = 0;
 
-    if (tree == NULL)
-        return (0);
-
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-
-    return (left_height > right_height ? left_height + 1 : right_height + 1);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((l > r) ? l : r);
+	}
 }
-
