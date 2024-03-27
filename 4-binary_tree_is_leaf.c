5a946b2867cf1cead4ca7b7b1e203d98bb29675c
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-03-27 05:01:22 -0700

    binary_trees

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,11 +9,9 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
     if (node == NULL)
-        return 0; // Return 0 if node is NULL
-
-    // Check if the node has no children
+        return 0;
     if (node->left == NULL && node->right == NULL)
-        return 1; // Return 1 if node is a leaf (no children)
+        return 1;
 
-    return 0; // Return 0 if node is not a leaf
+    return 0;
 }
