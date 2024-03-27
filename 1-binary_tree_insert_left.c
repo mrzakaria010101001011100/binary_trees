788bc998dbf7246ef77c69673ea4d55cce943b97
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-03-27 04:58:31 -0700

    binary_trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,7 +8,6 @@
  *
  * Return: Pointer to the created node, or NULL on failure or if parent is NULL
  */
-
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
@@ -23,7 +22,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
