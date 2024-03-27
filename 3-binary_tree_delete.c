c04503a129318937aa0e7212179352eaae64983f
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-03-27 04:46:23 -0700

    binary_trees

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -5,20 +5,21 @@
  * @tree: Pointer to the root node of the tree to delete
  * Return: nothing.
  */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return; // If tree is NULL, do nothing
-    }
-    else
+	if (tree == NULL)
+	{
+		return;
+	}
+	else
 	{
 		if (tree != NULL)
 		{
-            // Recursively delete the left and right
 			binary_tree_delete(tree->left);
 			binary_tree_delete(tree->right);
 		}
 		free(tree);
 	}
+
 }
